0cc197dfa67d90af1fd88c8df5744164ac0088f8
Joshua Granick <joshua@joshuagranick.com>
2013-06-06 18:29:27 -0700

    Use single format for now

--- NyanCat/Source/Main.hx
+++ NyanCat/Source/Main.hx
@@ -1,32 +1,4 @@
 package;
-#if !html5
-
-
-import flash.display.Sprite;
-import flash.utils.ByteArray;
-import format.SWF;
-
-@:file("library.swf") class Library extends ByteArray { }
-
-
-class Main extends Sprite {
-	
-	
-	public function new () {
-		
-		super ();
-		
-		var swf = new SWF (new Library ());
-		var cat = swf.createMovieClip ("NyanCatAnimation");
-		addChild (cat);
-		
-	}
-	
-	
-}
-
-
-#else // HTML5 does not support the above pattern yet
 
 
 import flash.display.Sprite;
@@ -48,6 +20,3 @@ class Main extends Sprite {
 	
 	
 }
-
-
-#end
