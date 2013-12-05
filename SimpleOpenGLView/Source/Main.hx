a022f51ab8e15a5ad2b17babe2a04ae5ca5c3d1e
Joshua Granick <joshua@joshuagranick.com>
2013-12-04 21:21:44 -0800

    Update SimpleOpenGLView sample, to display an image

--- SimpleOpenGLView/Source/Main.hx
+++ SimpleOpenGLView/Source/Main.hx
@@ -8,16 +8,25 @@ import openfl.display.OpenGLView;
 import openfl.gl.GL;
 import openfl.gl.GLBuffer;
 import openfl.gl.GLProgram;
+import openfl.gl.GLTexture;
 import openfl.utils.Float32Array;
+import openfl.utils.UInt8Array;
+import openfl.Assets;
 
 
 class Main extends Sprite {
 	
 	
+	private var imageUniform:Int;
+	private var modelViewMatrixUniform:Int;
+	private var projectionMatrixUniform:Int;
 	private var shaderProgram:GLProgram;
+	private var texCoordAttribute:Int;
+	private var texCoordBuffer:GLBuffer;
+	private var texture:GLTexture;
+	private var view:OpenGLView;
 	private var vertexAttribute:Int;
 	private var vertexBuffer:GLBuffer;
-	private var view:OpenGLView;
 	
 	
 	public function new () {
@@ -28,21 +37,10 @@ class Main extends Sprite {
 			
 			view = new OpenGLView ();
 			
-			createProgram ();
+			initializeShaders ();
 			
-			var vertices = [
-				
-				100, 100, 0,
-				-100, 100, 0,
-				100, -100, 0,
-				-100, -100, 0
-				
-			];
-			
-			vertexBuffer = GL.createBuffer ();
-			GL.bindBuffer (GL.ARRAY_BUFFER, vertexBuffer);	
-			GL.bufferData (GL.ARRAY_BUFFER, new Float32Array (cast vertices), GL.STATIC_DRAW);
-			GL.bindBuffer (GL.ARRAY_BUFFER, null);
+			createBuffers ();
+			createTexture ();
 			
 			view.render = renderView;
 			addChild (view);
@@ -52,17 +50,66 @@ class Main extends Sprite {
 	}
 	
 	
-	private function createProgram ():Void {
+	private function createBuffers ():Void {
+		
+		var vertices = [
+			
+			200, 200, 0,
+			-200, 200, 0,
+			200, -200, 0,
+			-200, -200, 0
+			
+		];
+		
+		vertexBuffer = GL.createBuffer ();
+		GL.bindBuffer (GL.ARRAY_BUFFER, vertexBuffer);
+		GL.bufferData (GL.ARRAY_BUFFER, new Float32Array (cast vertices), GL.STATIC_DRAW);
+		GL.bindBuffer (GL.ARRAY_BUFFER, null);
+		
+		var texCoords = [
+			
+			1, 1, 
+			0, 1, 
+			1, 0, 
+			0, 0, 
+			
+		];
+		
+		texCoordBuffer = GL.createBuffer ();
+		GL.bindBuffer (GL.ARRAY_BUFFER, texCoordBuffer);	
+		GL.bufferData (GL.ARRAY_BUFFER, new Float32Array (cast texCoords), GL.STATIC_DRAW);
+		GL.bindBuffer (GL.ARRAY_BUFFER, null);
+		
+	}
+	
+	
+	private function createTexture ():Void {
+		
+		texture = GL.createTexture ();
+		var bitmapData = Assets.getBitmapData ("assets/openfl.png");
+		GL.bindTexture (GL.TEXTURE_2D, texture);
+		GL.texImage2D (GL.TEXTURE_2D, 0, GL.RGBA, bitmapData.width, bitmapData.height, 0, GL.RGBA, GL.UNSIGNED_BYTE, new UInt8Array(bitmapData.getPixels (bitmapData.rect)));
+		GL.texParameteri (GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
+		GL.texParameteri (GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.LINEAR);
+		GL.bindTexture (GL.TEXTURE_2D, null);
+		
+	}
+	
+	
+	private function initializeShaders ():Void {
 		
 		var vertexShaderSource = 
 			
-			"attribute vec3 vertexPosition;
+			"attribute vec3 aVertexPosition;
+			attribute vec2 aTexCoord;
+			varying vec2 vTexCoord;
 			
-			uniform mat4 modelViewMatrix;
-			uniform mat4 projectionMatrix;
+			uniform mat4 uModelViewMatrix;
+			uniform mat4 uProjectionMatrix;
 			
 			void main(void) {
-				gl_Position = projectionMatrix * modelViewMatrix * vec4(vertexPosition, 1.0);
+				vTexCoord = aTexCoord;
+				gl_Position = uProjectionMatrix * uModelViewMatrix * vec4 (aVertexPosition, 1.0);
 			}";
 		
 		var vertexShader = GL.createShader (GL.VERTEX_SHADER);
@@ -77,12 +124,15 @@ class Main extends Sprite {
 		
 		var fragmentShaderSource = 
 			
-			"void main(void) {
-				gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+			"varying vec2 vTexCoord;
+			uniform sampler2D uImage0;
+			
+			void main(void)
+			{
+				gl_FragColor = texture2D (uImage0, vTexCoord).gbar;
 			}";
 		
 		var fragmentShader = GL.createShader (GL.FRAGMENT_SHADER);
-		
 		GL.shaderSource (fragmentShader, fragmentShaderSource);
 		GL.compileShader (fragmentShader);
 		
@@ -103,39 +153,53 @@ class Main extends Sprite {
 			
 		}
 		
-		vertexAttribute = GL.getAttribLocation (shaderProgram, "vertexPosition");
+		vertexAttribute = GL.getAttribLocation (shaderProgram, "aVertexPosition");
+		texCoordAttribute = GL.getAttribLocation (shaderProgram, "aTexCoord");
+		projectionMatrixUniform = GL.getUniformLocation (shaderProgram, "uProjectionMatrix");
+		modelViewMatrixUniform = GL.getUniformLocation (shaderProgram, "uModelViewMatrix");
+		imageUniform = GL.getUniformLocation (shaderProgram, "uImage0");
+		
+		GL.enableVertexAttribArray (vertexAttribute);
+		GL.enableVertexAttribArray (texCoordAttribute);
 		
 	}
+	
 	
 	
 	private function renderView (rect:Rectangle):Void {
 		
 		GL.viewport (Std.int (rect.x), Std.int (rect.y), Std.int (rect.width), Std.int (rect.height));
 		
-		GL.clearColor (8 >> 8, 146 >> 8, 208 >> 8, 1);
+		GL.clearColor (1.0, 1.0, 1.0, 1.0);
 		GL.clear (GL.COLOR_BUFFER_BIT);
 		
 		var positionX = rect.width / 2;
 		var positionY = rect.height / 2;
-		
 		var projectionMatrix = Matrix3D.createOrtho (0, rect.width, rect.height, 0, 1000, -1000);
 		var modelViewMatrix = Matrix3D.create2D (positionX, positionY, 1, 0);
 		
 		GL.useProgram (shaderProgram);
-		GL.enableVertexAttribArray (vertexAttribute);
+		
+		GL.activeTexture (GL.TEXTURE0);
+		GL.bindTexture (GL.TEXTURE_2D, texture);
+		GL.enable (GL.TEXTURE_2D);
 		
 		GL.bindBuffer (GL.ARRAY_BUFFER, vertexBuffer);
 		GL.vertexAttribPointer (vertexAttribute, 3, GL.FLOAT, false, 0, 0);
-		
-		var projectionMatrixUniform = GL.getUniformLocation (shaderProgram, "projectionMatrix");
-		var modelViewMatrixUniform = GL.getUniformLocation (shaderProgram, "modelViewMatrix");
+		GL.bindBuffer (GL.ARRAY_BUFFER, texCoordBuffer);
+		GL.vertexAttribPointer (texCoordAttribute, 2, GL.FLOAT, false, 0, 0);
 		
 		GL.uniformMatrix3D (projectionMatrixUniform, false, projectionMatrix);
 		GL.uniformMatrix3D (modelViewMatrixUniform, false, modelViewMatrix);
+		GL.uniform1i (imageUniform, 0);
 		
 		GL.drawArrays (GL.TRIANGLE_STRIP, 0, 4);
 		
 		GL.bindBuffer (GL.ARRAY_BUFFER, null);
+		GL.disable (GL.TEXTURE_2D);
+		GL.bindTexture (GL.TEXTURE_2D, null);
+		GL.activeTexture (null);
+		GL.useProgram (null);
 		
 	}
 	
