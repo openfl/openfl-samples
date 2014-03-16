b6ee7d5a9587b666b2fd1795c2f385c518206d95
Hasufel <Hasufel@users.noreply.github.com>
2014-03-16 08:40:08 +0100

    Fix indentation for easier reading

--- BouncingSquares/Source/Main.hx
+++ BouncingSquares/Source/Main.hx
@@ -18,11 +18,12 @@ class Main extends Sprite {
 
 		super();
 		
-		haxe.Timer.delay(init,100);
+		haxe.Timer.delay( init, 100 );
 
 	}
 
 	private function init():Void {
+	
 		this.mouseEnabled = false;
         	this.mouseChildren = false;
 		//gather stage dimensions to work with
@@ -33,53 +34,57 @@ class Main extends Sprite {
 		_squares = [];
 		
 		//prepare boucing squares
-		for (i in 0..._amount) {
-			var n:BouncingSquare = new BouncingSquare(randomNumber(200,_stageW-200),randomNumber(200,_stageH-200),randomNumber(1,10)*(randomNumber(0,1)>0?1:-1),randomNumber(1,10)*(randomNumber(0,1)>0?1:-1),randomNumber(0,0xffffff));
-			addChild(n);
-			_squares.push(n);
+		for ( i in 0..._amount ) {
+			var n:BouncingSquare = new BouncingSquare( randomNumber( 200, _stageW-200 ), randomNumber( 200, _stageH-200 ),randomNumber( 1, 10 )*( randomNumber( 0, 1 ) > 0 ?1:-1), randomNumber( 1, 10 ) * ( randomNumber( 0, 1 ) > 0?1:-1 ), randomNumber( 0, 0xffffff ) );
+			addChild( n );
+			_squares.push( n );
 		}
 
 		//add onEnterFrame listener
-		Lib.current.stage.addEventListener (Event.ENTER_FRAME, onEnterFrame);		
+		Lib.current.stage.addEventListener( Event.ENTER_FRAME, onEnterFrame );		
 	}
 	
-	private function onEnterFrame(e:Event):Void {
+	private function onEnterFrame( e:Event ):Void {
 		//handle move logic
-		for (i in 0..._amount) {
+		for ( i in 0..._amount ) {
 			var n:BouncingSquare = _squares[i];
-			if (n.x < 0 || n.x+n.width > _stageW) n.sx *= -1; //constraint horizontal translation
-			if (n.y < 0 || n.y+n.height > _stageH) n.sy *= -1; //constraint vertical translation
-			n.x+=n.sx;
-			n.y+=n.sy;
+			if ( n.x < 0 || n.x + n.width > _stageW ) n.sx *= -1; //constraint horizontal translation
+			if ( n.y < 0 || n.y + n.height > _stageH ) n.sy *= -1; //constraint vertical translation
+			n.x += n.sx;
+			n.y += n.sy;
 		}
 	}
 
-	private function randomNumber(low:Int, high:Int):Int {
+	private function randomNumber( low:Int, high:Int ):Int {
 		//return a random integer
-		return Math.round(Math.random() * (high - low) + low);
+		return Math.round( Math.random() * ( high - low ) + low );
 	}
 
 }
 
 class BouncingSquare extends Sprite {
-    public var sx:Int; //horizontal translation
-    public var sy:Int; //vertical translation
-    public var col:Int; //color
-
-    public function new(_x:Int,_y:Int,_sx:Int,_sy:Int,_col:Int) {
-    	super();
-    	x = _x; //x is already defined, as this is a sprite
-    	y = _y; //y is already defined too
-    	sx = _sx;
-    	sy = _sy;
-    	col = _col;
-    	init();
-    }
-
-    private function init ():Void {
-        this.addChild(new Bitmap(new BitmapData(10,10,false,col)));
-        this.mouseEnabled = false;
-        this.mouseChildren = false;
-    }
+	
+	public var sx:Int; //horizontal translation
+	public var sy:Int; //vertical translation
+	public var col:Int; //color
+	
+	public function new ( _x:Int, _y:Int, _sx:Int, _sy:Int, _col:Int ) {
+    	
+    		super();
+    	
+    		x = _x; //as this is extends Sprite, x is already a defined Sprite property, 
+    		y = _y; //as well as y
+    		sx = _sx;
+    		sy = _sy;
+    		col = _col;
+    	
+    		init();
+    	}
+    	
+    	private function init():Void {
+    		this.addChild( new Bitmap( new BitmapData(10, 10, false, col) ) );
+    		this.mouseEnabled = false;
+    		this.mouseChildren = false;
+    	}
 
 }
