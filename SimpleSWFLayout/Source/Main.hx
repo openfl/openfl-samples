7769682fac1477f58540adff7c1a2e0547b54fed
Joshua Granick <jgranick@users.noreply.github.com>
2015-02-06 16:53:53 -0800

    Fix for layout 1.2

--- SimpleSWFLayout/Source/Main.hx
+++ SimpleSWFLayout/Source/Main.hx
@@ -1,7 +1,7 @@
 package;
 
 
-import layout.LayoutGroup;
+import layout.Layout;
 import layout.LayoutItem;
 import openfl.display.MovieClip;
 import openfl.display.Sprite;
@@ -13,24 +13,24 @@ import openfl.Assets;
 class Main extends Sprite {
 	
 	
-	private var layout:MovieClip;
-	private var layoutGroup:LayoutGroup;
+	private var clip:MovieClip;
+	private var layout:Layout;
 	
 	
 	public function new () {
 		
 		super ();
 		
-		var layout = Assets.getMovieClip ("layout:Layout");
-		addChild (layout);
+		var clip = Assets.getMovieClip ("layout:Layout");
+		addChild (clip);
 		
-		layoutGroup = new LayoutGroup (800, 600);
+		layout = new Layout ();
 		
-		layoutGroup.addItem (new LayoutItem (layout.getChildByName ("Background"), STRETCH, STRETCH, false, false));
-		layoutGroup.addItem (new LayoutItem (layout.getChildByName ("Header"), STRETCH, TOP, false));
-		layoutGroup.addItem (new LayoutItem (layout.getChildByName ("Column"), LEFT, STRETCH, true, false));
+		layout.addItem (new LayoutItem (clip.getChildByName ("Background"), STRETCH, STRETCH, false, false));
+		layout.addItem (new LayoutItem (clip.getChildByName ("Header"), TOP, STRETCH, true, false));
+		layout.addItem (new LayoutItem (clip.getChildByName ("Column"), STRETCH, LEFT, false, true));
 		
-		layoutGroup.resize (stage.stageWidth, stage.stageHeight);
+		layout.resize (stage.stageWidth, stage.stageHeight);
 		stage.addEventListener (Event.RESIZE, stage_onResize);
 		
 	}
@@ -38,7 +38,7 @@ class Main extends Sprite {
 	
 	private function stage_onResize (event:Event):Void {
 		
-		layoutGroup.resize (stage.stageWidth, stage.stageHeight);
+		layout.resize (stage.stageWidth, stage.stageHeight);
 		
 	}
 	
