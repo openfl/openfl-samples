753f761920c3ed6f609a19d19b1d618d40b38cc6
Joshua Granick <jgranick@users.noreply.github.com>
2015-09-10 15:39:57 -0700

    Update BunnyMark to use TileMap

--- demos/BunnyMark/Source/Bunny.hx
+++ demos/BunnyMark/Source/Bunny.hx
@@ -1,24 +1,19 @@
 package;
 
 
-import flash.geom.Point;
+import openfl.display.Tile;
 
 
-class Bunny {
+class Bunny extends Tile {
 	
 	
-	public var alpha:Float;
-	public var position:Point;
-	public var rotation:Float;
-	public var scale:Float;
 	public var speedX:Float;
 	public var speedY:Float;
 	
 	
 	public function new () {
 		
-		speedX = 0;
-		speedY = 0;
+		super ();
 		
 	}
 	
---
+++ demos/BunnyMark/Source/Main.hx
@@ -0,0 +1,157 @@
+package;
+
+
+import openfl.display.FPS;
+import openfl.display.Sprite;
+import openfl.display.Tile;
+import openfl.display.TileMap;
+import openfl.display.TileMapLayer;
+import openfl.display.TileSet;
+import openfl.events.Event;
+import openfl.events.MouseEvent;
+import openfl.Assets;
+
+
+class Main extends Sprite {
+	
+	
+	private var addingBunnies:Bool;
+	private var bunnies:Array<Bunny>;
+	private var fps:FPS;
+	private var gravity:Float;
+	private var minX:Int;
+	private var minY:Int;
+	private var maxX:Int;
+	private var maxY:Int;
+	private var tileMap:TileMap;
+	private var tileMapLayer:TileMapLayer;
+	private var tileSet:TileSet;
+	
+	
+	public function new () {
+		
+		super ();
+		
+		bunnies = new Array ();
+		
+		minX = 0;
+		maxX = stage.stageWidth;
+		minY = 0;
+		maxY = stage.stageHeight;
+		gravity = 0.5;
+		
+		var bitmapData = Assets.getBitmapData ("assets/wabbit_alpha.png");
+		tileSet = new TileSet (bitmapData);
+		tileSet.addRect (bitmapData.rect);
+		
+		tileMapLayer = new TileMapLayer (tileSet);
+		
+		tileMap = new TileMap (stage.stageWidth, stage.stageHeight);
+		tileMap.addLayer (tileMapLayer);
+		addChild (tileMap);
+		
+		fps = new FPS ();
+		addChild (fps);
+		
+		stage.addEventListener (MouseEvent.MOUSE_DOWN, stage_onMouseDown);
+		stage.addEventListener (MouseEvent.MOUSE_UP, stage_onMouseUp);
+		stage.addEventListener (Event.ENTER_FRAME, stage_onEnterFrame);
+		
+		for (i in 0...100) {
+			
+			addBunny ();
+			
+		}
+		
+	}
+	
+	
+	private function addBunny ():Void {
+		
+		var bunny = new Bunny ();
+		bunny.x = 0;
+		bunny.y = 0;
+		bunny.speedX = Math.random () * 5;
+		bunny.speedY = (Math.random () * 5) - 2.5;
+		bunnies.push (bunny);
+		tileMapLayer.addTile (bunny);
+		
+	}
+	
+	
+	
+	
+	// Event Handlers
+	
+	
+	
+	
+	private function stage_onEnterFrame (event:Event):Void {
+		
+		for (bunny in bunnies) {
+			
+			bunny.x += bunny.speedX;
+			bunny.y += bunny.speedY;
+			bunny.speedY += gravity;
+			
+			if (bunny.x > maxX) {
+				
+				bunny.speedX *= -1;
+				bunny.x = maxX;
+				
+			} else if (bunny.x < minX) {
+				
+				bunny.speedX *= -1;
+				bunny.x = minX;
+				
+			}
+			
+			if (bunny.y > maxY) {
+				
+				bunny.speedY *= -0.8;
+				bunny.y = maxY;
+				
+				if (Math.random () > 0.5) {
+					
+					bunny.speedY -= 3 + Math.random () * 4;
+					
+				}
+				
+			} else if (bunny.y < minY) {
+				
+				bunny.speedY = 0;
+				bunny.y = minY;
+				
+			}
+			
+		}
+		
+		if (addingBunnies) {
+			
+			for (i in 0...100) {
+				
+				addBunny ();
+				
+			}
+			
+		}
+		
+	}
+	
+	
+	private function stage_onMouseDown (event:MouseEvent):Void {
+		
+		addingBunnies = true;
+		
+	}
+	
+	
+	private function stage_onMouseUp (event:MouseEvent):Void {
+		
+		addingBunnies = false;
+		trace (bunnies.length + " bunnies");
+		
+	}
+	
+	
+}
