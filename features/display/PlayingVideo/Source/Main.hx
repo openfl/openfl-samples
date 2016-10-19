a9d6d5fad5cbf694e87a812adfd7ae55b95c3385
Joshua Granick <jgranick@users.noreply.github.com>
2016-10-19 10:20:37 -0700

    Add 'PlayingVideo' sample

---
+++ features/display/PlayingVideo/Source/Main.hx
@@ -0,0 +1,53 @@
+package;
+
+
+import openfl.display.Sprite;
+import openfl.events.AsyncErrorEvent;
+import openfl.media.Video;
+import openfl.net.NetConnection;
+import openfl.net.NetStream;
+
+
+class Main extends Sprite {
+	
+	
+	private var netStream:NetStream;
+	private var video:Video;
+	
+	
+	public function new () {
+		
+		super ();
+		
+		video = new Video ();
+		addChild (video);
+		
+		var netConnection = new NetConnection ();
+		netConnection.connect (null);
+		
+		netStream = new NetStream (netConnection);
+		netStream.client = { onMetaData: client_onMetaData };
+		netStream.addEventListener (AsyncErrorEvent.ASYNC_ERROR, netStream_onAsyncError); 
+		netStream.play ("assets/example.mp4");
+		
+	}
+	
+	
+	private function client_onMetaData (metaData:Dynamic) {
+		
+		video.attachNetStream (netStream);
+		
+		video.width = video.videoWidth;
+		video.height = video.videoHeight;
+		
+	}
+	
+	
+	private function netStream_onAsyncError (event:AsyncErrorEvent):Void {
+		
+		trace ("Error loading video");
+		
+	}
+	
+	
+}
