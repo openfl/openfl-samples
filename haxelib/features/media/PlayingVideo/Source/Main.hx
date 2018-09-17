6a2c41dd9fdc11b897bc1a0bbc57c81e41d99a34
Joshua Granick <jgranick@users.noreply.github.com>
2018-09-17 15:56:00 -0700

    Quick fixes for PlayingVideo on HTML5

--- haxelib/features/media/PlayingVideo/Source/Main.hx
+++ haxelib/features/media/PlayingVideo/Source/Main.hx
@@ -1,8 +1,11 @@
 package;
 
 
+import motion.Actuate;
 import openfl.display.Sprite;
 import openfl.events.AsyncErrorEvent;
+import openfl.events.MouseEvent;
+import openfl.events.NetStatusEvent;
 import openfl.media.Video;
 import openfl.net.NetConnection;
 import openfl.net.NetStream;
@@ -12,6 +15,7 @@ class Main extends Sprite {
 	
 	
 	private var netStream:NetStream;
+	private var overlay:Sprite;
 	private var video:Video;
 	
 	
@@ -27,8 +31,19 @@ class Main extends Sprite {
 		
 		netStream = new NetStream (netConnection);
 		netStream.client = { onMetaData: client_onMetaData };
-		netStream.addEventListener (AsyncErrorEvent.ASYNC_ERROR, netStream_onAsyncError); 
+		netStream.addEventListener (AsyncErrorEvent.ASYNC_ERROR, netStream_onAsyncError);
+		
+		#if (js && html5)
+		overlay = new Sprite ();
+		overlay.graphics.beginFill (0, 0.5);
+		overlay.graphics.drawRect (0, 0, 560, 320);
+		overlay.addEventListener (MouseEvent.MOUSE_DOWN, overlay_onMouseDown);
+		addChild (overlay);
+		
+		netConnection.addEventListener (NetStatusEvent.NET_STATUS, netConnection_onNetStatus);
+		#else
 		netStream.play ("assets/example.mp4");
+		#end
 		
 	}
 	
@@ -46,6 +61,25 @@ class Main extends Sprite {
 	private function netStream_onAsyncError (event:AsyncErrorEvent):Void {
 		
 		trace ("Error loading video");
+		
+	}
+	
+	
+	private function netConnection_onNetStatus (event:NetStatusEvent):Void {
+		trace (event.info.code);
+		if (event.info.code == "NetStream.Play.Complete") {
+			
+			Actuate.tween (overlay, 1, { alpha: 1 });
+			
+		}
+		
+	}
+	
+	
+	private function overlay_onMouseDown (event:MouseEvent):Void {
+		
+		Actuate.tween (overlay, 2, { alpha: 0 });
+		netStream.play ("assets/example.mp4");
 		
 	}
 	
